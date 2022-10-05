e5fda485f29a28c5306cdc23c3f522db9a813759
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-10-05 15:05:53 -0500

    Update alldone.py

--- alldone.py
+++ alldone.py
@@ -128,9 +128,9 @@ def append_objs_to_img(cv2_im, inference_size, objs, labels,colors_array,values)
         percent = int(100 * obj.score)
         label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
 
-        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-        cv2_im = cv2.putText(cv2_im, prediction, (x0, y0+30),
-                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+        #cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        #cv2_im = cv2.putText(cv2_im, prediction, (x0, y0+30),
+        #                     cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
         colors.append(prediction)
         
     print(colors)

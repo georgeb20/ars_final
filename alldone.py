494d27f7dd2332bddf96fdad28aec1f1facc9e45
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-10-05 14:07:52 -0500

    Update alldone.py

--- alldone.py
+++ alldone.py
@@ -98,13 +98,13 @@ def main():
             break
         cv2_im = frame
         height=640
-        if(is_good_photo(cv2_im, height, mean, sliding_window)):
-            cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-            cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
-            run_inference(interpreter, cv2_im_rgb.tobytes())
-            objs = get_objects(interpreter, args.threshold)[:args.top_k]
-            cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels,colors_array,values)
-            cv2.imshow('frame', cv2_im)
+     #   if(is_good_photo(cv2_im, height, mean, sliding_window)):
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+        run_inference(interpreter, cv2_im_rgb.tobytes())
+        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels,colors_array,values)
+        cv2.imshow('frame', cv2_im)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 

64fda1ff8ee993402da8d375f820405b5e71e534
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-01 16:55:19 -0500

    Update uart.py

--- uart.py
+++ uart.py
@@ -5,12 +5,13 @@ def resistance2array(resistance):
     first_digit = string_res[0]
     second_digit = string_res[1]
     num_zeros = str(len(string_res[2:]))
-    return [first_digit+second_digit+num_zeros]
+    return first_digit+second_digit+num_zeros
 
 serial = Serial("/dev/ttymxc2", 9600)
 
 
 resistance_array = resistance2array(2200)
+
 serial.write(bytes(resistance_array,'utf-8'))
 # Open /dev/ttyUSB0 with baudrate 115200, and defaults of 8N1, no flow control
 

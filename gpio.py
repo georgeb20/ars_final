bbba536bb46a4a6e5d3059b24c6aa04eb32ddd11
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-04 19:38:17 -0500

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,10 +1,10 @@
 from periphery import GPIO
-import time
-solenoid = GPIO("/dev/gpiochip2", 13, "out")  # pin 37
+
+led = GPIO("/dev/gpiochip2", 13, "out")  # pin 37
 
 try:
   while True:
-    solenoid.write(True)
+    led.write(True)
 finally:
-  solenoid.write(False)
-  solenoid.close()
+  led.write(False)
+  led.close()

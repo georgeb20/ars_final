bfc3c3ec8375536d1ed1a68b451ac7bdb4e245c9
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-04 20:18:54 -0500

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,11 +1,6 @@
 from periphery import GPIO
 
-led = GPIO("/dev/gpiochip0", 6, "out")  # pin 37
+led = GPIO("/dev/gpiochip2", 13, "out")  # pin 37
 
-try:
-  while True:
+while True:
     led.write(True)
-finally:
-  led.write(False)
-  led.close()
-  button.close()

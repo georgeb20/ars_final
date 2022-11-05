a3e6404ddf8536786230778ec612ae0f2a9f1896
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-04 19:55:13 -0500

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -1,10 +1,10 @@
 from periphery import GPIO
 
-gpio_p16 = GPIO("/dev/gpiochip2", 9, "out")
+gpio = GPIO("/sys/class/gpio/gpio77", 13, "out")
 
 try:
   while True:
-    gpio_p16.write(True)
+    gpio.write(True)
 finally:
-  gpio_p16.write(False)
-  gpio_p16.close()
+  gpio.write(False)
+  gpio.close()

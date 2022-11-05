14a2a97af74cb6f5f5d2b962e389aeb3ca47284c
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-04 19:00:22 -0500

    Update headless.py

--- headless.py
+++ headless.py
@@ -1,5 +1,4 @@
 import argparse
-import colorsys
 import cv2
 import os
 
@@ -8,55 +7,25 @@ from pycoral.adapters.detect import get_objects
 from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
-#
-import cv2
 from color_recognition_api import color_histogram_feature_extraction
 from color_recognition_api import knn_classifier
-import os
 import os.path
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
 import numpy as np
-import tflite_runtime.interpreter as tflite 
-from pycoral.adapters import common
-from pycoral.adapters import detect
-from pycoral.utils.dataset import read_label_file
-import random
-import os
-from color_recognition_api import color_histogram_feature_extraction
-from color_recognition_api import knn_classifier
-import cv2
-
-
-from multiprocessing import cpu_count
 
 from scipy import ndimage
+from periphery import Serial
 
 
-#from utils import CameraWebsocketHandler
-#from utils.BiQuad import BiQuadFilter
-#from functools import partial
-#from PIL import Image
-#from scipy import ndimage
-#import edgetpu.classification.engine
-#import threading
-#import asyncio
-#import base64
-#import utils
-#import cv2
-#import argparse
-#import sys
-#import RPi.GPIO as GPIO
 
 
+
+serial = Serial("/dev/ttymxc2", 9600)
 
 def main():
     
     default_model_dir = '.'
-    default_model = 'model.tflite'
-    default_labels = 'band-labels.txt'
+    default_model = 'band80.tflite'
+    default_labels = 'labels.txt'
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='.tflite model path',
                         default=os.path.join(default_model_dir,default_model))
@@ -65,12 +34,12 @@ def main():
     parser.add_argument('--top_k', type=int, default=4,
                         help='number of categories with highest score to display')
     parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 1)
-    parser.add_argument('--threshold', type=float, default=0.1,
+    parser.add_argument('--threshold', type=float, default=0.2,
                         help='classifier score threshold')
     args = parser.parse_args()
 
-    colors_array = ["black","brown","red","orange","yellow","green","blue","violet","gold"]
-    values = [0,1,2,3,4,5,6,7,-1]
+    colors_array = ["black","brown","red","orange","yellow","green","blue","violet","grey","white","gold"]
+    values = [0,1,2,3,4,5,6,7,8,9,-1]
 
     prediction = 'n.a.'
     mean = [None]
@@ -102,43 +71,45 @@ def main():
             cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
             cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
             run_inference(interpreter, cv2_im_rgb.tobytes())
-            objs = get_objects(interpreter, args.threshold)[:args.top_k]
-            get_resistance(cv2_im, inference_size, objs, labels,colors_array,values)
-            #cv2.imshow('frame', cv2_im)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            objs = get_objects(interpreter, args.threshold)
+            append_objs_to_img(cv2_im, inference_size, objs,colors_array,values)
 
+    serial.close()
     cap.release()
     cv2.destroyAllWindows()
+    
 
-def get_resistance(cv2_im, inference_size, objs, labels,colors_array,values):
+def resistance2array(resistance):
+    string_res = str(resistance)
+    first_digit = string_res[0]
+    second_digit = string_res[1]
+    num_zeros = str(len(string_res[2:]))
+    return first_digit,second_digit,num_zeros
+
+def append_objs_to_img(cv2_im, inference_size, objs,colors_array,values):
     height, width, channels = cv2_im.shape
     scale_x, scale_y = width / inference_size[0], height / inference_size[1]
     colors=[]
     for obj in objs:
         bbox = obj.bbox.scale(scale_x, scale_y)
 
-
         x0, y0 = int(bbox.xmin), int(bbox.ymin)
         x1, y1 = int(bbox.xmax), int(bbox.ymax)
-        
-        band_crop = cv2_im[x0:x1,y0:y1]
-        band_crop = cv2.cvtColor(np.array(band_crop), cv2.COLOR_RGB2BGR)
+
+
+        dx = x1-x0
+        dy = y1-y0
+
+        band_crop = cv2_im[y0:y0+dy,x0:x0+dx]
         color_histogram_feature_extraction.color_histogram_of_test_image(band_crop)
         prediction = knn_classifier.main('training.data', 'test.data')
-        
-       # percent = int(100 * obj.score)
-       # label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
-
-    #    cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-    #    cv2_im = cv2.putText(cv2_im, prediction, (x0, y0+30),
-      #                       cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
         colors.append(prediction)
-
+        
     resistance = color2res(colors,colors_array,values)
-    #cv2_im = cv2.putText(cv2_im, str(resistance), (30, 30),
-   #                         cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
     print(resistance)
+    #resistance_array = resistance2array(resistance)
+    #serial.write(bytes(resistance_array,'utf-8'))
+    print(colors)
 
 def is_good_photo(img, height, mean, sliding_window):
     threshold = 4.5
@@ -155,15 +126,16 @@ def is_good_photo(img, height, mean, sliding_window):
         sliding_window.append(detection_zone_avg)
     # print(detection_zone_avg)
     if mean[0] != None and abs(detection_zone_avg - mean[0]) > threshold:
-        print("Target Detected Taking Picture")
         return True
 
     return False
 def color2res(bands,colors,values):
+    if "unknown" in bands:
+        return 0
     colors.reverse()
     flag=0
     if len(bands)==4 or len(bands)==5:
-        if(bands[0]=="Gold"):
+        if(bands[0]=="gold"):
           bands.reverse()
           flag=1  
         if(len(bands)==4):

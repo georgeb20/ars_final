5c59a78a2e27c88d94de29cb71057467a27af6c4
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-11 18:17:35 -0600

    Update gig.py

--- gig.py
+++ gig.py
@@ -1,21 +1,46 @@
-from skimage.metrics import structural_similarity as compare_ssim
 import cv2
+from scipy import ndimage
+import numpy as np
+import scipy
+filter_type = 'zone'
+def is_good_photo(img, width, height, mean, sliding_window):
+    detection_zone_height = 20
+    detection_zone_interval = 5
+    threshold = 4.5
+    if (filter_type == 'zone'):
+        detection_zone_avg = img[height // 2 : (height // 2) + detection_zone_height : detection_zone_interval, 0:-1:3].mean()
+    if (filter_type == 'biquad2d'):
+        detection_zone_avg = abs(bq.process(img.mean))
+    if (filter_type == 'biquad'):
+        detection_zone_avg = abs(bq.process(img[height // 2: (height // 2) + detection_zone_height: detection_zone_interval, 0:-1:3].mean()))
+    if (filter_type == 'center_of_mass'):
+        center = scipy.ndimage.measurements.center_of_mass(img)
+        detection_zone_avg = (center[0] + center[1]) / 2
 
 
+    if len(sliding_window) > 30:
+        mean[0] = np.mean(sliding_window)
+        sliding_window.clear()
 
+    else:
+        sliding_window.append(detection_zone_avg)
+    # print(detection_zone_avg)
+    if mean[0] != None and abs(detection_zone_avg - mean[0]) > threshold:
+        print("Target Detected Taking Picture")
+        return True
+
+    return False
+
+
+mean = [None]
+sliding_window = []
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
-    ret,frame1 = cap.read()
+    ret,frame = cap.read()
+    height,width,channel = frame.shape
+    if(is_good_photo(frame,width,height,mean,sliding_window)):
+        print(1)
 
-    ret, frame2 = cap.read()
-
-    if not ret:
-        break
-    grayA = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    grayB = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    (score) = compare_ssim(grayA, grayB, full=True)
-    if(score<.90):
-       print("SSIM: {}".format(score))
 
 
 

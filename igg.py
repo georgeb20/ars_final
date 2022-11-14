2bdfaf605a9ba73000d17ac75c771dc94d30c426
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-13 19:56:31 -0600

    Update igg.py

--- igg.py
+++ igg.py
@@ -17,8 +17,6 @@ while(True):
         print("Started recording.")
         led.write(True)
         sleep(2)
-    else:
-        led.write(False)
     last_mean= np.mean(gray)
     if (cv2.waitKey(1) & 0xFF == ord('q')):
         break

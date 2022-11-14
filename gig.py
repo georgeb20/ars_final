189fec78a1a0c39d2dac2086b45b089d409fcfac
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-13 19:29:54 -0600

    Update gig.py

--- gig.py
+++ gig.py
@@ -2,7 +2,7 @@ import cv2
 from scipy import ndimage
 import numpy as np
 from time import sleep
-filter_type = 'center_of_mass'
+filter_type = 'zone'
 def is_good_photo(img, height, mean, sliding_window):
     detection_zone_height = 20
     detection_zone_interval = 5

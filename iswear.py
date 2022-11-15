6b682187edd53ad7a0115609c8f33dc89c6c9760
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-15 15:38:21 -0600

    Update iswear.py

--- iswear.py
+++ iswear.py
@@ -2,16 +2,13 @@ import cv2
 # define a video capture object
 cap = cv2.VideoCapture(0)  
 
-cap.set(3, 640) # set the resolution
-cap.set(4, 480)
-cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-focus=0
+#cap.set(3, 640) # set the resolution
+#cap.set(4, 480)
 while(True):
       
     # Capture the video frame
     # by frame
-    focus = input("Enter a focus")
-    cap.set(28,focus)
+
     ret, frame = cap.read()
   
     # Display the resulting frame

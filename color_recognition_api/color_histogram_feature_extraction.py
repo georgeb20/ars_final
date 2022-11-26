9dfa3b080f33f1e055e17e0e4590d684ae56ae6e
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-11-26 14:12:03 -0600

    Update color_histogram_feature_extraction.py

--- color_recognition_api/color_histogram_feature_extraction.py
+++ color_recognition_api/color_histogram_feature_extraction.py
@@ -14,8 +14,11 @@ from color_recognition_api import knn_classifier as knn_classifier
 def removeGlare(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     mask = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)[1]
-    result = cv2.inpaint(img, mask, 21, cv2.INPAINT_TELEA)
-    return result
+    try:
+        result = cv2.inpaint(img, mask, 21, cv2.INPAINT_TELEA)
+        return result
+    except:
+        return img
 
 
 def center_crop(img, dim):
